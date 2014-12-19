dbdb56925087a8b20f2077544bd42a2dd549c8af
felipejsad <felipejsad03@gmail.com>
2014-12-19 18:36:07 -0500

    Escena de Candado
    He creado la escena del candado rotando

---
+++ Assets/DesaparecerTextoEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DesaparecerTextoEventHandler : MonoBehaviour, ITrackableEventHandler {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	#region ITrackableEventHandler implementation
+
+	public void OnTrackableStateChanged (TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+	{
+		// si el trackeable aparece
+			
+
+		// desaparecer texto
+		// si trackeable desaparece
+		// Aparecer Texto
+	}
+
+	#endregion
+}
