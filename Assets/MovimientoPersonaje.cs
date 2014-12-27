904d45e15bfb949f9a1d1f95f16d22f2804bee27
HendrysTobar <fabiantobar@gmail.com>
2014-12-26 19:10:23 -0500

    Programé el comportamiento del elfo del primer nivel
    En la escena "Pagina1"  se da click en el piso de la casita y el elfo se
    muebe en esa direccion.
    No está probado con RA

---
+++ Assets/MovimientoPersonaje.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class MovimientoPersonaje : MonoBehaviour {
+
+	CharacterController cc;
+	public float speed;
+	Plane groundPlane;
+	Vector3 destination;
+	// Use this for initialization
+	void Start () 
+	{
+		cc = GetComponent<CharacterController>();
+		groundPlane = new Plane(transform.parent.up, transform.parent.position);
+		destination = transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 d = Capture3DClickPosition();
+		if(d != Vector3.zero)
+		{
+			destination = d;
+		}
+
+		Vector3 vel = destination - transform.position;
+
+		cc.SimpleMove(speed * vel.normalized * Time.deltaTime);
+
+	
+	}
+
+
+	Vector3 Capture3DClickPosition ()
+	{
+		if (Input.GetMouseButtonDown(0)) 
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			float rayDistance;
+			if (groundPlane.Raycast(ray, out rayDistance))
+			{
+				return ray.GetPoint(rayDistance);
+			}
+			else
+			{
+				return Vector3.zero;
+			}
+
+		}
+		else
+		{
+			return Vector3.zero;
+		}
+
+	}
+}
+
